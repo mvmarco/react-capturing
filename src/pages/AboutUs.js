8261a55840c402d49d7b3b1b7cceee50dff1e778
marco corapi <marco.corapi@outlook.it>
2021-04-01 18:30:58 +0200

    created about us compoents, and about section micro-component

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+// page AboutUs' components:
+import AboutSection from '../components/AboutSection';
+
+const AboutUS = () => {
+  return (
+    <div>
+      <AboutSection />  
+    </div>
+  )
+}
+
+export default AboutUS;
