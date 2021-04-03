b8718def21fcff639139443b48a40943c570d2d0
marco corapi <marco.corapi@outlook.it>
2021-04-03 18:37:22 +0200

    creating ourwork components

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -4,15 +4,57 @@ import styled from "styled-components";
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
-// Link from react dom, when you click image you go to another page
+// Link from react dom, when you click image you go to another page with the actual movie
 import { Link } from "react-router-dom";
 
 const OurWork = () => {
   return (
-    <div>
-      <h1>Our Work</h1>
-    </div>
-  )
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="athlete movie" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="racer movie" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="good times movie" />
+        </Link>
+      </Movie>
+    </Work>
+  );
 }
+// styled component
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+  `;
 
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+`;
 export default OurWork;
