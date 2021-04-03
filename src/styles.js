9ad503fcc0b23a69b6784552c3fe320a41ccd4dc
marco corapi <marco.corapi@outlook.it>
2021-04-03 10:03:24 +0200

    styles.js reusable components

---
+++ src/styles.js
@@ -0,0 +1,6 @@
+/* 
+  The difference is that globalStyle file's purpose is for only styling things 
+  like the HTML, body, *, etc. It's not meant to be reused, only done once.
+  However, the Style file is for reusable styles for descriptions, images, etc. 
+  that may be imported to multiple components to be reused across the project.
+*/
