b0692c4c6b52097cc8b177713e1eecf1ec689405
marco corapi <marco.corapi@outlook.it>
2021-04-03 18:10:02 +0200

    router path and to links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 // import components
 import AboutUs from './pages/AboutUs';
 import Nav from "./components/Nav";
-import ContactUs from "../components/ContactUs";
-import OurWork from "../components/OurWork";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 // import global style
 import GlobalStyle from './components/GlobalStyle';
 // router
+import { Switch, Route, useLocation } from "react-router-dom";
+/* 
+  we import Route, to speficy where to render the component inside ot if.
+  But it does not give an exact route. If you wrote the path="/"
+  and another path="/work" react will render both, because it notices the "/" as first
+  match.
+
+  With "switch" the component get rendered as soon as it matches the first url or the first path,
+  so if it matches the slash is not going to the other ones (paths or url) anymore. It is
+  not going to work or contacts. As soon as it matches the first one, is gonna stop and render
+  the component and it is gonna ignore everything else. So you wrap the in the "switch" 
+  all the "route".
+
+  We you use "exact" on the slash path, to say: render this specific "/" as the exact path
+  and if you don't find it render the others, because now the others are not anymore exactly
+  a single /.
+*/
 function App() {
   return (
     <div className="App">
-      <Nav />
       <GlobalStyle />
-      <AboutUs />
-      <OurWork/>
-      <ContactUs />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contacts">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
