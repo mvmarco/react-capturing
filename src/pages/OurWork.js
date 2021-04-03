37eb1c81cd8d43a5c4ee95af2aaab266dbf46944
marco corapi <marco.corapi@outlook.it>
2021-04-03 18:14:43 +0200

    creating ourwork components

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,3 +1,12 @@
+// import styled components
+import styled from "styled-components";
+// images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+// Link from react dom, when you click image you go to another page
+import { Link } from "react-router-dom";
+
 const OurWork = () => {
   return (
     <div>
