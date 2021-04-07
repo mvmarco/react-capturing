75db0b1819dec01d0829f20e5a74e6f161c29af6
marco corapi <marco.corapi@outlook.it>
2021-04-07 17:22:46 +0200

    animation photo scale

--- src/animation.js
+++ src/animation.js
@@ -37,3 +37,15 @@ export const fade = {
     transition: { ease: "easeOut", duration: 0.75 },
   },
 };
+
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
+    },
+  },
+};
