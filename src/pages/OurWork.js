de0b4a76227e9a0538ca7d7cfbbd5b3b2acbba3f
marco corapi <marco.corapi@outlook.it>
2021-04-15 12:24:47 +0200

    animation second movie

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -16,8 +16,11 @@ import {
   slider,
   sliderContainer,
 } from "../animation";
+import {useScroll} from '../components/useScroll';
 
 const OurWork = () => {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
   return (
     <Work variants={pageAnimation} initial="hidden" animate="show" exit="exit">
       <motion.div variants={sliderContainer}> 
@@ -35,7 +38,7 @@ const OurWork = () => {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+      <Movie ref={element} variants={fade} animate={controls} initial='hidden'>
         <h2>The Racer</h2>
         <div className="line"></div>
         <Link to="/work/the-racer">
@@ -63,7 +66,7 @@ const Work = styled(motion.div)`
   }
   `;
 
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
