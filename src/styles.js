e8e44755d4b5d60219c5a94b11a7c285baa59b63
marco corapi <marco.corapi@outlook.it>
2021-04-16 12:35:46 +0200

    styling nav

--- src/styles.js
+++ src/styles.js
@@ -18,7 +18,7 @@ export const Layout = styled(motion.div)`
   justify-content:space-between;
   padding: 5rem 10rem;
   color: #ffffff;
-  @media (max-width: 1300px){
+  @media (max-width: 1200px){
     display: block;
     padding: 2rem 2rem;
     text-align: center;
@@ -31,6 +31,12 @@ export const Description = styled.div`
   z-index: 2;
   h2{
     font-weight: lighter;
+  }
+    @media (max-width: 1200px) {
+    padding: 0;
+    button {
+      margin: 2rem 0rem 5rem 0rem;
+    }
   }
 `
 
