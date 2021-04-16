b3933039e8d5fd4b8348754e1f8e28a35aedf22a
marco corapi <marco.corapi@outlook.it>
2021-04-16 13:22:33 +0200

    fixin styling faq

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -94,7 +94,7 @@ const Faq = styled(Layout)`
       padding: 1rem 0rem;
     }
   }
-    @media (max-width: 1300px){
+    @media (max-width: 1200px){
     display: block;
     padding: 2rem 2rem;
     text-align: center;
